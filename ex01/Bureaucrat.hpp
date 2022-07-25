0942ea73369b236565cc1cfa39e89920288e80aa
sohan <sohan@student.42seoul.kr>
2022-07-25 20:22:13 +0900

    [feat] ex01

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,63 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+# include <exception>
+# include "Form.hpp"
+
+# define GRADE_LOWEST	150
+# define GRADE_HIGHEST	1
+
+class	Form;
+
+class	Bureaucrat{
+
+private:
+	const std::string	name;
+	int					grade;
+
+	Bureaucrat();
+public:
+	
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat& ref);
+	Bureaucrat&	operator=(const Bureaucrat& ref);
+
+	const std::string	getName(void) const;
+	int					getGrade(void) const;
+	void				increaseGrade(void);
+	void				decreaseGrade(void);
+
+	void				signForm(Form& form);
+
+class GradeTooHighException : virtual public std::exception{
+
+public:
+	explicit GradeTooHighException() {}
+
+	virtual ~GradeTooHighException() throw() {}
+
+	virtual const char*	what() const throw () {
+		return ("Grade Too High!");
+	}
+};
+
+class GradeTooLowException : virtual public std::exception{
+
+public:
+	explicit GradeTooLowException() {}
+
+	virtual ~GradeTooLowException() throw() {}
+
+	virtual const char*	what() const throw () {
+		return ("Grade Too Low!");
+	}
+};
+
+};
+
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
---
+++ ex01/Form.cpp
@@ -0,0 +1,80 @@
+#include "Form.hpp"
+
+Form::Form()
+	:signGrade(-1), executeGrade(-1)
+{
+
+}
+
+Form::Form(std::string name, const int sg, const int eg)
+	:name(name), isSigned(false), signGrade(sg), executeGrade(eg)
+{
+	if (this->signGrade < GRADE_HIGHEST || this->executeGrade < GRADE_HIGHEST)
+		throw GradeTooHighException();
+	if (this->signGrade > GRADE_LOWEST || this->executeGrade > GRADE_LOWEST)
+		throw GradeTooLowException();
+}
+
+Form::Form(const Form& ref)
+	:name(ref.getName()), isSigned(false), signGrade(ref.getSignGrade()), executeGrade(ref.getExecuteGrade())
+{
+	if (this->signGrade < GRADE_HIGHEST || this->executeGrade < GRADE_HIGHEST)
+		throw GradeTooHighException();
+	if (this->signGrade > GRADE_LOWEST || this->executeGrade > GRADE_LOWEST)
+		throw GradeTooLowException();
+}
+
+Form&	Form::operator=(const Form& ref)
+{
+	if (this != &ref)
+	{
+		*(const_cast<std::string*>(&this->name)) = ref.getName();
+		this->isSigned = ref.getIsSigned();
+		*(const_cast<int*>(&this->signGrade)) = ref.getSignGrade();
+		*(const_cast<int*>(&this->executeGrade)) = ref.getExecuteGrade();
+	}
+	return (*this);
+}
+
+void	Form::beSigned(const Bureaucrat& bureaucrat)
+{
+	if (bureaucrat.getGrade() > this->signGrade)
+		throw GradeTooHighException();
+	this->isSigned = true;
+	//*(const_cast<bool*>(&this->isSigned)) = true;
+}
+
+const std::string&	Form::getName(void) const
+{
+	return (this->name);
+}
+
+const bool&	Form::getIsSigned(void) const
+{
+	return (this->isSigned);
+}
+
+const int&		Form::getSignGrade(void) const
+{
+	return (this->signGrade);
+}
+
+const int&		Form::getExecuteGrade(void) const
+{
+	return (this->executeGrade);
+}
+
+std::ostream&	operator<<(std::ostream& out, const Form& form)
+{
+	out << "[Form]" << "\n"
+		<< "Name          : " << form.getName() << "\n"
+		<< "Signed        : " << form.getIsSigned() << "\n"
+		<< "Sign    grade : " << form.getSignGrade() << "\n"
+		<< "Execute grade : " << form.getExecuteGrade() << std::endl;
+	return (out);
+}
+
+Form::~Form()
+{
+
+}
